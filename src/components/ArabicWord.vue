e82d77e44e3959b45712f43fee7997c2dd888802
Julian Rüth <julian.rueth@fsfe.org>
2022-07-19 00:19:09 +0300

    Extend list of special arabic characters

--- src/components/ArabicWord.vue
+++ src/components/ArabicWord.vue
@@ -47,6 +47,31 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+const special = new RegExp(`(?=[^${[
+  '\u064B', // Fathatan
+  '\u064C', // Dammatan
+  '\u064D', // Kasratan
+  '\u064E', // Fatha
+  '\u064F', // Damma
+  '\u0650', // Kasra
+  '\u0651', // Shadda
+  '\u0652', // Sukun
+  '\u0653', // Maddah Above
+  '\u0654', // Hamza Above
+  '\u0655', // Hamza Below
+  '\u0656', // Subscript Alef
+  '\u0657', // Inverted Damma
+  '\u0658', // Mark Noon Ghunna
+  '\u0659', // Zwarakay
+  '\u065A', // Vowel Sign Small V Above
+  '\u065B', // Vowel Sign Inverted Small V Above
+  '\u065C', // Vowel Sign Dot Below
+  '\u065D', // Reversed Damma
+  '\u065E', // Fatha With Two Dots
+  '\u065F', // Wavy Hamza Below
+  '\u0670', // Letter Superscript Alef
+].join('')}])`);
+
 export default defineComponent({
   props: {
     arabic: {
@@ -57,7 +82,7 @@ export default defineComponent({
   },
   computed: {
     arabics() {
-      return this.arabic.trim().split(/(?=[^ ؘ َ  ُ  ِ  ّ  ْ  ٓ  ٔ  ٕ])/)
+      return this.arabic.trim().split(special)
     },
     arabic_lights() {
       let light = false;
